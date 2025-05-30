60a62da3693a4ac31ee7f58ab3bee181117b9e8b
Adrian Paredes <adrianparedesroman99@gmail.com>
2025-05-30 18:00:00 -0400

    using mysql and setting up redis for real time db and auth/session storage

--- myproject/trivia/settings.py
+++ myproject/trivia/settings.py
@@ -81,10 +81,25 @@ WSGI_APPLICATION = 'trivia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "spotify",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
